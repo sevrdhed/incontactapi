8595606c801972270ec757e8fd61ca618f39afb6
Steve Heath <sheath6633@yahoo.com>
2018-04-18 18:37:19 +0000

    Changed call retrieval service to only pull for yesterday and today rather than a hardcoded date

--- callretrieval.rb
+++ callretrieval.rb
@@ -1,28 +1,35 @@
 #  Call Recording top level retrieval service
 
-# Dependencies on the other methods in this folder. Need to be changed when migrated to the EC2 instance to reflect the correct filepath
-require '/mnt/c/Users/stephen.heath/Ruby/incontactapi/contactscompleted.rb'
-require '/mnt/c/Users/stephen.heath/Ruby/incontactapi/getfiles.rb'
+# Dependencies on the other methods in this folder. 
+require '/home/ubuntu/Ruby/incontactapi/contactscompleted.rb'
+require '/home/ubuntu/Ruby/incontactapi/getfiles.rb'
 
 require 'base64'
 download_array = []
 
-#Get the list of contacts. Currently this date value is hardcoded. TO DO: Change this to pull calls for prior day.
-contacts_hash = getCompletedContacts('2018-02-27', '2018-02-28')
+# This creates the two variables used to pass in yesterday and todays date. This kinda feels like a hack, particularly yesterday.
+today = Time.now.strftime('%Y-%m-%d')
+yesterday = Time.now - 86400
+yesterday = yesterday.strftime('%Y-%m-%d')
+
+
+#Get the list of contacts for yesterday and today
+contacts_hash = getCompletedContacts(yesterday, today)
 
 #This parses through each contact to see if it was logged or not. If it's logged, add it to the list to be downloaded.
 contacts_hash["completedContacts"].each do |i|
-	if i["isLogged"] == true
-	download_array << i["contactId"]
-	end
+    if i["isLogged"] == true
+      download_array << i["contactId"]
+    end
+   
   end
   
   #Get the first file from the array. Currently this value is hardcoded. TO DO: Change this to a loop that pulls all the data for the entire array
-  encoded_data_hash = getFile(download_array[0])
-  encoded_data = encoded_data_hash["files"]["file"]
+ # encoded_data_hash = getFile(download_array[0])
+ # encoded_data = encoded_data_hash["files"]["file"]
   
   #Write the file to the server
-  File.open("calltest.wav", "w+") {|f| f.write(Base64.decode64(encoded_data)) }
+ # File.open("calltest.wav", "w+") {|f| f.write(Base64.decode64(encoded_data)) }
   
   #This is just for testing in IRB to confirm that the services have run correctly and gathered the necessary information into my download list
   puts download_array
